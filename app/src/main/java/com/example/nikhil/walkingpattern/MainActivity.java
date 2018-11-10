b5ae4448a9d4ad25ff9a01b5423777c8e8d477cf
Nikhil Wagh <nikhilwagh.work@gmail.com>
2018-11-10 23:10:49 +0530

    Added behavioural support for checkboxes in Navigation Drawer in Main Activity.

--- app/src/main/java/com/example/nikhil/walkingpattern/MainActivity.java
+++ app/src/main/java/com/example/nikhil/walkingpattern/MainActivity.java
@@ -20,17 +20,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.jjoe64.graphview.GraphView;
@@ -45,8 +46,14 @@ public class MainActivity extends AppCompatActivity
 
     private Intent collectDataIntent;
     private final String TAG = "NavigationDrawer";
-    private LineGraphSeries<DataPoint> mSeries1;
+    private LineGraphSeries<DataPoint> mSeries;
 
+    private String currentAxis = "";
+    private final int X_AXIS_INDEX = 0;
+    private final int Y_AXIS_INDEX = 1;
+    private final int Z_AXIS_INDEX = 2;
+
+    private Query getAccInOrder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,8 +84,8 @@ public class MainActivity extends AppCompatActivity
 
         collectDataIntent = new Intent(this, CollectDataService.class);
         GraphView graphView = findViewById(R.id.dynamicGraph);
-        mSeries1 = new LineGraphSeries<>();
-        graphView.addSeries(mSeries1);
+        mSeries = new LineGraphSeries<>();
+        graphView.addSeries(mSeries);
 
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -87,8 +94,9 @@ public class MainActivity extends AppCompatActivity
                 .build();
         db.setFirestoreSettings(settings);
 
-        CollectionReference accCollectionReference = db.collection("AccelerometerReadings");
-        accCollectionReference.orderBy("createdAt").addSnapshotListener(new EventListener<QuerySnapshot>() {
+        getAccInOrder = db.collection("AccelerometerReadings").orderBy("createdAt");
+
+        getAccInOrder.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
                 if (e != null) {
@@ -102,7 +110,7 @@ public class MainActivity extends AppCompatActivity
                             minX = (long) doc.get("createdAt");
                         currX = (long) doc.get("createdAt") - minX;
                         if (currX > prevX) {
-                            mSeries1.appendData(new DataPoint(currX, (double) doc.get("x_axis")), false, 100);
+                            mSeries.appendData(new DataPoint(currX, (double) doc.get(getCurrentAxis())), false, 100);
                             prevX = currX;
                         } else {
                             Log.e(TAG, "next x-axis value is not greater n\n" + currX + " : " + prevX);
@@ -157,8 +165,9 @@ public class MainActivity extends AppCompatActivity
         userEmailIDTV.setText(currentUser.getEmail());
 
         MenuItem menuItem = navigationView.getMenu().findItem(R.id.checkboxX_axis);
-        CompoundButton customSwitch = (CompoundButton) menuItem.getActionView();
-        customSwitch.setChecked(true);
+        CompoundButton compundButton = (CompoundButton) menuItem.getActionView();
+        compundButton.setChecked(true);
+
 
     }
 
@@ -203,14 +212,27 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        final CompoundButton compundButton = (CompoundButton) item.getActionView();
 
+        showSnackBar(String.valueOf(item.getGroupId()));
         if (id == R.id.checkboxX_axis) {
-            item.getActionView().setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+            if (compundButton.isChecked()) {
+                setCurrentAxis(X_AXIS_INDEX);
+            }
+        }
+        else if (id == R.id.checkboxY_axis) {
+            if (compundButton.isChecked()) {
+                setCurrentAxis(Y_AXIS_INDEX);
+            }
+        }
+        else if (id == R.id.checkboxZ_axis) {
+            if (compundButton.isChecked()) {
+                setCurrentAxis(Z_AXIS_INDEX);
+            }
+        }
 
-                }
-            });
+        if (shouldResetGraph()) {
+            resetGraph();
         }
         /*if (id == R.id.nav_camera) {
             // Handle the camera action
@@ -229,5 +251,46 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void resetGraph() {
+        final DataPoint[] points = new DataPoint[1000];
+        getAccInOrder.limit(1000).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.getResult() != null && task.isSuccessful()) {
+                            int index = 0;
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                points[index] = new DataPoint((float) doc.get("createdAtMillis"), (float) doc.get(getCurrentAxis()));
+                                index++;
+                            }
+                        }
+                        else {
+                            Log.e(TAG, "Complete database GET FAILED", task.getException());
+                        }
+                    }
+                });
+        mSeries = new LineGraphSeries<>(points);
+    }
+
+    private boolean shouldResetGraph() {
+        //TODO: Replace with proper logic
+        return true;
+    }
+
+    private void setCurrentAxis(int axis) {
+        if (axis == X_AXIS_INDEX)
+            currentAxis = "x_axis";
+        else if (axis == Y_AXIS_INDEX)
+            currentAxis = "y_axis";
+        else if (axis == Z_AXIS_INDEX)
+            currentAxis = "z_axis";
+        else
+            currentAxis = "x_axis";
+    }
+
+    private String getCurrentAxis() {
+        return currentAxis;
     }
 }
